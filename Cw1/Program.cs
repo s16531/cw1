df01a71e8dc3deaa89b9f6fd2a608763bd141da2
s16531 <s16531@LAB214-22>
2020-02-29 21:26:31 +0100

    Koniec zadania

---
+++ Cw1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace Cw1
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
+            var client =new HttpClient();
+            var result =await client.GetAsync(url);
+
+            if(result.IsSuccessStatusCode)
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(html);
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+            }
+            
+            Console.WriteLine("Hello World!");
+        }
+
+      
+    }
+}
